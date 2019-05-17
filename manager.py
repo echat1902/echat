02f37f8ee263de7a54b09e5d804da50ef245b822
tglogin <tglogin@163.com>
2019-05-17 19:37:08 +0800

    扯淡

--- manager.py
+++ manager.py
@@ -1,43 +1,7 @@
 # 管理和启动项目
 from app import create_app
-from flask import render_template, request, session, redirect, make_response, abort
-
-from flask_socketio import SocketIO
-import json
 
 app = create_app()
-socketio = SocketIO(app)
-
-# @app.route('/')
-# @app.route('/index/')
-# def index():
-#     return render_template('flasksocketio.html')
-
-
-@socketio.on('imessage',namespace='/flasksocketio')
-def test_message(message):
-    socketio.emit('server_response',json.dumps({'data':message}),namespace='/flasksocketio')
-
-
-# @socketio.on('single chat',namespace='/chat')
-# def single_chat():
-#     data = {'xm': 'hello'}
-#     socketio.emit('server_response', data)
-
-
-@socketio.on('connect')
-def connect():
-    pass
-
-
-@socketio.on('disconnect')
-def dis_connect():
-    pass
-
-
-
-
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0')
-    socketio.run(app,debug=True)
+    app.run(host='0.0.0.0')
